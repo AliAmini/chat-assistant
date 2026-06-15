a0590d35e26f4c5084b25690858d35afb1d82399
Ali Amini <aliamini.af@gmail.com>
2026-06-15 17:13:14 +0330

    chore: hide lint error

--- apps/backend/src/prisma/prisma.service.ts
+++ apps/backend/src/prisma/prisma.service.ts
@@ -8,10 +8,12 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$connect();
   }
 
   async onModuleDestroy() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     await this.$disconnect();
   }
 }
